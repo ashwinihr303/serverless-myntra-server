a064c585ea3f7c3f46fb959314b4125c9bf1f704
ashwinihr303 <ashwinigowda2204@gmail.com>
2023-05-04 16:29:03 +0530

    Dynamodb implementation

--- src/user/users.js
+++ src/user/users.js
@@ -12,6 +12,9 @@ const {
 const { fakeDB } = require("../utils/fakeDB");
 const { isAuth } = require("../utils/isAuth");
 const { getSecrets } = require("../utils/loadSecrets");
+const AWS = require("aws-sdk");
+const USERS_TABLE = process.env.users_table;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 
 // 1. Register a user
 router.post("/register", async (req, res) => {
@@ -124,4 +127,54 @@ router.get("/test", async (req, res) => {
   const secret = await getSecrets("secrets_access_key");
   return res.send({ msg: `Secrete value ${secret}` });
 });
+
+router.get("/users/:userId", async function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  try {
+    const { Item } = await dynamoDbClient.get(params).promise();
+    if (Item) {
+      const { userId, name } = Item;
+      res.json({ userId, name });
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive user" });
+  }
+});
+
+router.post("/users", async function (req, res) {
+  const { userId, name } = req.body;
+  if (typeof userId !== "string") {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== "string") {
+    res.status(400).json({ error: '"name" must be a string' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId: userId,
+      name: name,
+    },
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    res.json({ userId, name });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create user" });
+  }
+});
+
 module.exports = router;
